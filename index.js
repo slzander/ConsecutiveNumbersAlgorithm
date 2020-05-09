a7cb9aea77822895b55f6f83a639eaaedffd1a59
slzander <staceyzander@gmail.com>
2020-05-09 12:14:05 -0600

    changes variable and function names, adds comments

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
-function consecutiveNumbers (number, factor = 1) {
+function sumOfConsecutiveNumbers (number, factor = 1) {
     // We need to find an odd factor of the original number.
     // If a number is odd, the number itself is an odd factor
 
+    // If the number is odd
     if (number % 2 == 1){
         return createOutputArray(number, factor)
         
@@ -13,10 +14,10 @@ function consecutiveNumbers (number, factor = 1) {
         number = number / 2
 
         if (number != 1) {
-            //recursively loop back through to see if we now have an odd number
-            return consecutiveNumbers(number, factor)
+            // Recursively loop back through to see if we now have an odd number
+            return sumOfConsecutiveNumbers(number, factor)
 
-        // If number divides down to equal 1, it is a power of 2 and there are no odd factors,
+        // If input number divides down to equal 1, it's a power of 2 and there are no odd factors,
         // which means it will not work for this problem
         } else {
             return "There is no consecutive sequence of numbers that sum to equal your input."
@@ -25,14 +26,15 @@ function consecutiveNumbers (number, factor = 1) {
     
 }
 
-function createOutputArray(number, spread) {
+function createOutputArray(oddFactor, spread) {
     let output = []
-    let startNumber = Math.floor(number / 2)
+    // The centerpoint of our output is half of the odd factor
+    let startNumber = Math.floor(oddFactor / 2)
 
-    // If the number is a multiple of 4, we often get negative numbers in the output,
+    // If spread is greater than the startNumber, we get 0 or negative numbers in the output,
     // So we need to follow this pattern to produce only positive numbers
     if (spread > startNumber) {
-        for (let i = 0; i < number; i++){
+        for (let i = 0; i < oddFactor; i++){
             output.push(spread - startNumber + i)
         }
     // All other inputs can follow this pattern
@@ -44,14 +46,14 @@ function createOutputArray(number, spread) {
     return output
 }
 
-console.log('Input:3 Output:',consecutiveNumbers(3))
-console.log('Input:6 Output:',consecutiveNumbers(6))
-console.log('Input:8 Output:',consecutiveNumbers(8))
-console.log('Input:10 Output:',consecutiveNumbers(10))
-console.log('Input:18 Output:',consecutiveNumbers(18))
-console.log('Input:24 Output:',consecutiveNumbers(24))
-console.log('Input:32 Output:',consecutiveNumbers(32))
-console.log('Input:35 Output:',consecutiveNumbers(35))
-console.log('Input:80 Output:',consecutiveNumbers(80))
-console.log('Input:292 Output:',consecutiveNumbers(292))
+console.log('Input:3 Output:',sumOfConsecutiveNumbers(3))
+console.log('Input:6 Output:',sumOfConsecutiveNumbers(6))
+console.log('Input:8 Output:',sumOfConsecutiveNumbers(8))
+console.log('Input:10 Output:',sumOfConsecutiveNumbers(10))
+console.log('Input:18 Output:',sumOfConsecutiveNumbers(18))
+console.log('Input:24 Output:',sumOfConsecutiveNumbers(24))
+console.log('Input:32 Output:',sumOfConsecutiveNumbers(32))
+console.log('Input:35 Output:',sumOfConsecutiveNumbers(35))
+console.log('Input:80 Output:',sumOfConsecutiveNumbers(80))
+console.log('Input:292 Output:',sumOfConsecutiveNumbers(292))
 
